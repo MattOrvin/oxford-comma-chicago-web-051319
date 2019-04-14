ac631ea81de3d32e6c71afa89de9f728abc9c279
Matt Orvin <matthew.orvin@gmail.com>
2019-04-14 18:24:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-    if array.size >= 3
-      array.insert(-2, "and")
-      array.join(", ")
-    elsif array.size <2
+    if array.size <2
       array.join
-    else
+    elsif array.size == 2
       array.insert(-2, "and")
       array.join(" ")
+    else
+      array.insert(-2, "and")
+      array.join(", ")
 end
 end
